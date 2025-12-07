d72c45b94d5cfb6873081fb4e6f9ddbd9ac730e9
Shweta Belekar <belekarshwetagmail.com>
2025-12-08 12:33:53 +1300

    Automated test case for change password feautre

--- Pages/ChangePasswordPage.cs
+++ Pages/ChangePasswordPage.cs
@@ -17,6 +17,12 @@ namespace AdvanceProjectMars_Task6.Pages
         private IWebElement confirmPassword => Driver.FindElement(By.XPath("//input[@placeholder='Confirm Password']"));
 
         private IWebElement saveButton => Driver.FindElement(By.XPath("//button[@class='ui button ui teal button' and text()='Save']"));
+        private IWebElement signOutButton => Driver.FindElement(By.XPath("//button[@class='ui green basic button' and text()='Sign Out']"));
+        private IWebElement signinButton => Driver.FindElement(By.XPath("//a[@class='item' and text()='Sign In']"));
+        private IWebElement emailAddressTextbox => Driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[1]/input"));
+        private IWebElement passwordTextbox => Driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[2]/input"));
+        private IWebElement loginButton => Driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button"));
+        private IWebElement hitony => Driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/div[1]/div[2]/div/span"));
         public void CreateNewPassword(string CurrentPassword, string NewPassword, string ConfirmPassword)
         {
             currentPassword.SendKeys(CurrentPassword);
@@ -26,8 +32,19 @@ namespace AdvanceProjectMars_Task6.Pages
             confirmPassword.SendKeys(ConfirmPassword);
             Thread.Sleep(2000);
             saveButton.Click();
+            Wait.WaitToBeVisible(Driver, "XPath", "//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']", 2);
+            IWebElement popupAlert = Driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']"));
+            string alertText = popupAlert.Text;
+            Console.WriteLine("Alert text: " + alertText);
 
-
+        }
+        public void VerifySigninwithNewPassword(string NewPassword)
+        {
+            Thread.Sleep(2000);
+            signOutButton.Click();
+            Thread.Sleep(3000);
+            signinButton.Click();
+            emailAddressTextbox.SendKeys();
         }
     }
 }
